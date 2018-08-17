82191f449011e32ead0aa51926dd0b94d9edff80
valeria <a.valeria.koriatchenko@freenet.de>
2018-08-17 15:06:15 +0200

    Outsource google API functionalities

--- src/components/Map.js
+++ src/components/Map.js
@@ -2,10 +2,6 @@ import React, { Component } from "react";
 import { initMap } from "../utils/GoogleMapsAPI";
 
 class Map extends Component {
-  state = {
-    markers: [],
-    //infoWindow: new window.google.maps.InfoWindow(),
-  };
 
   componentDidMount() {
 
@@ -14,38 +10,6 @@ class Map extends Component {
 
     initMap(places, mapContainer);
   }
-
-
-  //Display InfoWindow
-  populateInfoWindow = e => {
-    //call FoursquareAPI and set content
-    //Template literal?
-    const { infoWindow, places, markers } = this.state;
-
-    infoWindow.setContent();
-    // can I call this.map?
-    // infoWindow.open(this.map, marker);
-
-
-    let selectedPlace = places.findIndex(place => place.name === e);
-
-    infoWindow.setContent(this.fetchContent(places[selectedPlace].requestId));
-    infoWindow.open(this.map, markers[selectedPlace]);
-
-    // infowindow.addListener('closeclick', function() {
-    //   infowindow.marker = null;
-    // });
-  };
-
-  //Hide filtered marker
-  filterMarker = () => {
-    if (this.props.query) {
-      // filteredMarkers = marker.filter(marker => !marker.title.includes(query))
-      // check if there is open InfoWindow on filteredMarker -> closeInfoWindow()
-    }
-  };
-
-  closeInfoWindow = () => {};
 
   render() {
     return (
