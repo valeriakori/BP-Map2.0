6db76043db3585154904023960965f7c11c637ec
valeria <a.valeria.koriatchenko@freenet.de>
2018-08-19 20:51:02 +0200

    Refactor to initially call and store photoUrls

--- src/utils/FoursquareAPI.js
+++ src/utils/FoursquareAPI.js
@@ -1,29 +1,56 @@
 import * as cred from "./crededentials.js";
+import myPlaces from "./GoogleMapsAPI";
+
+export let imageUrls = [
+  {
+    title: "Casa Baja",
+    photoUrl: "url"
+  },
+  {
+    title: "Linguarum",
+    photoUrl: "url"
+  }
+];
 
 // Returns requested venues' imageUrl
-export const fetchImage = venueID => {
+const fetchImage = () => {
   const requestURL = `https://api.foursquare.com/v2/venues/${venueID}/photos?limit=1&client_id=${
     cred.CLIENT_ID
   }&client_secret=${cred.CLIENT_SECRET}&v=${cred.version}`;
 
-  fetch(requestURL)
-    .then(response => response.json())
-    .then(getImageUrl)
-    .catch(error => console.log("Oopsie daisy, an error occured: " + error))
+  myPlaces.forEach(places => {
+    if (!places.requestURL) {
+      return;
+    } else {
+      venueID = places.requestURL;
+    }
+    fetch(requestURL)
+      .then(response => response.json())
+      .then(getImageUrl)
+      .catch(error => console.log("Oopsie daisy, an error occured: " + error));
+  });
 };
 
 const getImageUrl = data => {
-  let photo = ''
-  let photoUrl = "";
+  let imageUrl = {
+    title: "",
+    photoUrl: ""
+  };
+  let photo = "";
 
-//   if (data.response && data.response.items && data.response.items.length > 0) {
-  if (data.response && data.response.photos && data.response.photos.items && data.response.photos.items.length > 0) {
+  // if iteration of place is not reachable, maybe move this whole function to second then
+  console.log(place)
+  if (
+    data.response &&
+    data.response.photos &&
+    data.response.photos.items &&
+    data.response.photos.items.length > 0
+  ) {
+    imageUrl.title = place.title;
 
     photo = data.response.photos.items[0];
-    photoUrl = photo.prefix + "cap300" + photo.suffix;
-    console.log(photoUrl);
+    imageUrl.photoUrl = photo.prefix + "cap300" + photo.suffix;
 
-    return photoUrl;
+    imageUrls.push(imageUrl);
   }
 };
-
