d6c4f7ae77708863b3a7298959a647ac27aeca07
valeriakori <a.valeria.koriatchenko@gmail.com>
2018-08-21 17:38:21 +0200

    Function to generate fetch urls

--- src/utils/FoursquareAPI.js
+++ src/utils/FoursquareAPI.js
@@ -12,53 +12,72 @@ let imageUrls = [
   }
 ];
 
-// Returns requested venues' imageUrl
-const fetchImages = () => {
-  let venueID = "";
-  let requestURL = ''
-  // let requestURL = `https://api.foursquare.com/v2/venues/${venueID}/photos?limit=1&client_id=${
-  //   cred.CLIENT_ID
-  // }&client_secret=${cred.CLIENT_SECRET}&v=${cred.version}`;
 
-  myPlaces.forEach(place => {
-    requestURL = `https://api.foursquare.com/v2/venues/${venueID}/photos?limit=1&client_id=${
+
+const generateUrlArray = arr => {
+
+
+
+
+  let urlsToFetch = arr.map(place => {
+    let venueID = ''
+
+    let requestURL = `https://api.foursquare.com/v2/venues/${venueID}/photos?limit=1&client_id=${
       cred.CLIENT_ID
-    }&client_secret=${cred.CLIENT_SECRET}&v=${cred.version}`;
+      }&client_secret=${cred.CLIENT_SECRET}&v=${cred.version}`
+
     if (!place.requestId) {
       return;
     } else {
-      venueID = place.requestId;
-      //console.log(venueID)
+      venueID = place.requestId
+      console.log(requestURL)
+
     }
-    fetch(requestURL)
-      .then(response => {
-        response.json();
-      })
-      .then(data => {
-        console.log(data)
+  })
 
-        let imageUrl = {
-          title: "",
-          photoUrl: ""
-        };
-        let photo = "";
+  return urlsToFetch
 
-        if (
-          data.response &&
-          data.response.photos &&
-          data.response.photos.items &&
-          data.response.photos.items.length > 0
-        ) {
-          imageUrl.title = place.title;
+}
 
-          photo = data.response.photos.items[0];
-          imageUrl.photoUrl = photo.prefix + "cap300" + photo.suffix;
+// Returns requested venues' imageUrl
+const fetchImages = () => {
 
-          imageUrls.push(imageUrl);
-        }
-      })
-      .catch(error => console.log("Oopsie daisy, an error occured: " + error));
-  });
+  generateUrlArray(myPlaces)
+  /*  Promise.all(myPlaces.map(place => {
+ 
+     console.log(place.requestId)
+ 
+     fetch(requestURL)
+       .then(response => {
+         response.json();
+       })
+       //.then(getUrl)
+       .catch(error => console.log("Oopsie daisy, an error occured: " + error));
+   })) */
 };
+
+/* const getUrl = (data) =>  {
+  console.log(data)
+
+  let imageUrl = {
+    title: "",
+    photoUrl: ""
+  };
+  let photo = "";
+
+  if (
+    data.response &&
+    data.response.photos &&
+    data.response.photos.items &&
+    data.response.photos.items.length > 0
+  ) {
+    imageUrl.title = place.title;
+
+    photo = data.response.photos.items[0];
+    imageUrl.photoUrl = photo.prefix + "cap300" + photo.suffix;
+
+    imageUrls.push(imageUrl);
+  }
+} */
 
 export { imageUrls, fetchImages };
