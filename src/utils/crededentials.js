ee90c83c04af4db64febe4cab6e7bd77c6d6ab13
valeriakori <a.valeria.koriatchenko@gmail.com>
2018-08-22 11:14:24 +0200

    Add instruction for API keys

--- src/utils/crededentials.js
+++ src/utils/crededentials.js
@@ -1,33 +1,3 @@
-export const CLIENT_ID = "RFVKM0U1QKJALUHTOJDCP41XXYR4KKNX5YQGYBKGZ4CIIENY"
-export const CLIENT_SECRET = "YVAFWEYO2YFHRQVT2WK0BVPPOH2ZUXNA3W4KT13B2YTQDZQR"
+export const CLIENT_ID = "YOUR_CLIENT_ID_HERE"
+export const CLIENT_SECRET = "YOUR_CLIENT_SECRET_HERE"
 export const version = 20180821
-
-//,
-//   {
-//     "title": "Szimpla Kert",
-//     "subtitle": "Ruin Bar",
-//     "location": { "lat": 47.497013, "lng": 19.063314 },
-//     "requestId": "4b630e1af964a52020602ae3",
-//     "description": "The Szimpla Kert is located in the heart of Budapests 7th district, where most of the nightlife is going on. The ruin bar hosts many events such as flea markets, artisan markets, brunches, open mic nights and partys."
-//   },
-//   {
-//     "title": "Hősök Tere",
-//     "subtitle": "Hero Square",
-//     "location": { "lat": 47.514943, "lng": 19.077863 },
-//     "requestId": "4b6c6ae5f964a52082382ce3",
-//     "description": "Hősök tere is one of the major squares in Budapest, Hungary, noted for its iconic statue complex featuring the Seven chieftains of the Magyars and other important Hungarian national leaders, as well as the Tomb of the Unknown Soldier."
-//   },
-//   {
-//     "title": "Margitsziget",
-//     "subtitle": "Margit Island",
-//     "location": { "lat": 47.527607, "lng": 19.04696 },
-//     "requestId": "4bb25b49a32876b0dc7d00fe",
-//     "description": "Awesome retreat with a 5k running track, fountain shows and park directly on the Danube."
-//   },
-//   {
-//     "title": "Országház",
-//     "subtitle": "Parliament",
-//     "location": { "lat": 47.507121, "lng": 19.045669 },
-//     "requestId": "4bd6e74b5631c9b63889a630",
-//     "description": "Simply put the most amazing piece of architecture I had the honor of seeing. My motto for the time in Budapest was \"A day without a view on the Parliament, is a day bad spent.\" "
-//   }
